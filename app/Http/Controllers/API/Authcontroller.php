5134789fb1a7fb9169afe048533530a81f84bf03
Juan Carlo Castillo <juancarlo.castillo20@gmail.com>
2022-05-30 12:00:44 +0200

    implement AuthController

--- app/Http/Controllers/API/Authcontroller.php
+++ app/Http/Controllers/API/Authcontroller.php
@@ -1,10 +1,63 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use phpDocumentor\Reflection\Types\Null_;
 
-class Authcontroller extends Controller
+class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        
+        $validatedData = $request->validate([
+            'nickname' => 'nullable',
+            'email' => 'required | email | max:255 | unique:users',
+            'password' => 'required | confirmed',
+        ]);
+
+        if(!$validatedData['nickname'] | $validatedData['nickname'] == Null){ 
+            $validatedData['nickname'] = 'Anonimo';
+        }
+
+        if($validatedData['nickname'] = )
+
+        $validatedData['password'] =  Hash::make($request->password);
+
+        $user = User::create($validatedData);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response ([
+            'user' => $user,
+            'access_token' => $accessToken
+        ]);
+    }
+
+
+
+
+    public function login (Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'required | email ',
+            'password' => 'required '
+        ]);
+
+        if (!auth()->attempt($loginData)){
+            return response (['message' => 'Invalid login credentials'] , 422);
+        }
+
+        $accessToken = Auth::user()->createToken('userToken')->accessToken;
+
+
+        return response (['user' => Auth::user(), 'access_token' => $accessToken]);
+        
+        
+    }
 }
