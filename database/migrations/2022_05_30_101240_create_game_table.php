dfa4553700aa5b388bda6d526523f0411e7abab0
Juan Carlo Castillo <juancarlo.castillo20@gmail.com>
2022-05-30 12:13:37 +0200

    implement routes & migration

---
+++ database/migrations/2022_05_30_101240_create_game_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('game', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('jugador')->references('id')->on('users');
+            $table->tinyInteger('dado1');
+            $table->tinyInteger('dado2');
+            $table->string('resultado' , 11);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('game');
+    }
+};
