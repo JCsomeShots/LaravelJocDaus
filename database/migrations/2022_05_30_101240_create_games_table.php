6b9ebb80a0ad2a73afa84aadb25bd2b5082d472d
Juan Carlo Castillo <juancarlo.castillo20@gmail.com>
2022-05-31 21:46:04 +0200

    implement seeder game

--- database/migrations/2022_05_30_101240_create_games_table.php
+++ database/migrations/2022_05_30_101240_create_games_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('game', function (Blueprint $table) {
+        Schema::create('games', function (Blueprint $table) {
             $table->id();
             $table->tinyInteger('dado1');
             $table->tinyInteger('dado2');
